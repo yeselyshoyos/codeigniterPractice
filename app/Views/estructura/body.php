9eac58a0d65c301a2ab31492101a81fb9ca1df1c
yeselyshoyos <yeselys.depp@gmail.com>
2021-10-12 00:04:30 -0500

    nuevo

--- app/Views/estructura/body.php
+++ app/Views/estructura/body.php
@@ -1,6 +1,26 @@
 <body>
     <h4>Hola desde del body</h4>
-    <?php echo suma(1, 2); ?> <br>
-    <?php echo resta(2, 4); ?>
+    <!---<//?php echo suma(1, 2); ?> <br>
+    <//?php echo resta(2, 4); ?>-->
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>name</th>
+            <th>email</th>
+            <th>deleted</th>
+        </tr>
+        <tr>
+            <?php
+            foreach($users as $user){
+                echo "<tr>";
+                echo "<td>".$user['id']."</td>";
+                echo "<td>".$user['name']."</td>";
+                echo "<td>".$user['email']."</td>";
+                echo "<td>".$user['deleted_at']."</td>";
+                echo "</tr>";
+            }
+             ?>
+        </tr>
+    </table>
 </body>
 </html>
