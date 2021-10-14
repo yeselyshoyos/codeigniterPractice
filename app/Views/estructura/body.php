7a5abb0ac82fc55a6a79c7b1090686c8ac39006b
yeselyshoyos <yeselys.depp@gmail.com>
2021-10-14 18:53:38 -0500

    nuevo4

--- app/Views/estructura/body.php
+++ app/Views/estructura/body.php
@@ -2,7 +2,7 @@
     <div class="container">
         <div class="row">
             <a href="<?php base_url();?>./home/formulario" class="btn btn-info mt-10" role="button">Nuevo</a>
-        </div>
+        </div><br>
         <!---<h4>Hola desde del body</h4>
         <//?php echo suma(1, 2); ?> <br>
         <//?php echo resta(2, 4); ?>-->
@@ -18,10 +18,16 @@
                     <?php
                     foreach($users as $user){
                         echo "<tr scope='row'>";
-                        echo "<td>".$user['id']."</td>";
-                        echo "<td>".$user['name']."</td>";
-                        echo "<td>".$user['email']."</td>";
-                        echo "<td>".$user['deleted_at']."</td>";
+                            echo "<td>".$user['id']."</td>";
+                            echo "<td>".$user['name']."</td>";
+                            echo "<td>".$user['email']."</td>";
+                            echo "<td>".$user['deleted_at']."</td>";
+                            echo "<td>";
+                            ?>
+                            <a href="<?php base_url();?>./home/editar?id=<?php echo $user['id']?>" class="btn btn-warning" role="button"><i class="fa fa-pencil"></i></a>
+                            <a href="<?php base_url();?>./home/borrar?id=<?php echo $user['id']?>" class="btn btn-danger" role="button"><i class="fa fa-trash"></i></a>
+                            <?php
+                            echo "</td>";
                         echo "</tr>";
                     }
                     ?>
--- app/Views/estructura/formulario.php
+++ app/Views/estructura/formulario.php
@@ -1,14 +1,37 @@
+<div class="container">
 <?php   
 
 echo form_open('/home/guarda');
+if(isset($users)){
+    $name = $users[0]['name'];
+    $email = $users[0]['email'];
+}else{
+    $name = "";
+    $email = "";
+}
+?>
+<div class="form-group">
+<br>
+<?php
 echo form_label('Nombre ', 'name',);
-echo form_input(array('name' => 'name','placeholder' => 'Nombre',));
+echo form_input(array('name' => 'name','placeholder' => 'Nombre', 'class' => 'form-control', 'value'=> $name,));
 echo "<br>";
 echo "<br>";
 echo form_label('Correo Electronico ', 'email',);
-echo form_input(array('name' => 'email','placeholder' => 'Correo Electronico',));
+echo form_input(array('name' => 'email','placeholder' => 'Correo Electronico','class' => 'form-control','value'=> $email,));
 echo "<br>";
 echo "<br>";
-echo form_submit('guarda', 'Guardar');
+echo form_submit('guarda', 'Guardar','class="btn btn-primary"',);
+if(isset($users)){
+    echo form_hidden('id', $users[0]['id']);
+}else{
+    $name = "";
+    $email = "";
+}
+
+?>
+</div>
+<?php
 echo form_close();
 ?>
+</div>
